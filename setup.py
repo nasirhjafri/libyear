ab30b908f595bec9bb06d4d817cfbbd5df678941
Eric Casteleijn <thisfred@gmail.com>
2019-12-16 19:36:26 -0800

    Add a test that checks requirements files with hashes

--- setup.py
+++ setup.py
@@ -2,30 +2,30 @@
 
 from setuptools import setup
 
-
 setup(
-    name='libyear',
-    version='0.1.0',
-    description='A simple measure of software dependency freshness.',
-    long_description=open('README.md').read(),
-    author='nasirhjafri',
-    url='https://github.com/nasirhjafri/libyear',
+    name="libyear",
+    version="0.1.0",
+    description="A simple measure of software dependency freshness.",
+    long_description=open("README.md").read(),
+    author="nasirhjafri",
+    url="https://github.com/nasirhjafri/libyear",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: Other Environment',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
     ],
-    packages=['libyear'],
-    py_modules=['libyear'],
-    scripts=['libyear/libyear'],
-    dependency_links=[
-    ],
+    packages=["libyear"],
+    py_modules=["libyear"],
+    scripts=["libyear/libyear"],
+    dependency_links=[],
     install_requires=[
-        'requests==2.22.0',
-        'prettytable==0.7.2',
-        'python-dateutil==2.8.1',
+        "requests==2.22.0",
+        "prettytable==0.7.2",
+        "python-dateutil==2.8.1",
     ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
 )
