8885bf36328a9519b2ac7ec446dd3d5b5b6b7119
Nasir Hussain <nasirhjafri@gmail.com>
2020-11-05 15:47:05 +0500

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="libyear",
-    version="0.2.0",
+    version="0.2.1",
     description="A simple measure of software dependency freshness.",
     long_description=long_description,
     long_description_content_type='text/markdown',
